7da3c7052cffce9439b5734c1450e31a4f1d1581
Gabriele Catania <gabriele.ctn@gmail.com>
2021-02-12 17:30:03 +0000

    reformat

--- src/main/java/it/gcatania/holidaytemps/HolidayTempsApplication.java
+++ src/main/java/it/gcatania/holidaytemps/HolidayTempsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HolidayTempsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HolidayTempsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HolidayTempsApplication.class, args);
+    }
 
 }

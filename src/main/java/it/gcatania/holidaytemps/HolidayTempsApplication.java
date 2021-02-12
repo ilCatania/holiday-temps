1d5a7c44795bb6d39c6b3f0f39da4cd362edc476
Gabriele Catania <gabriele.ctn@gmail.com>
2021-02-12 15:22:47 +0000

    boilerplate from sping boot initializr

---
+++ src/main/java/it/gcatania/holidaytemps/HolidayTempsApplication.java
@@ -0,0 +1,13 @@
+package it.gcatania.holidaytemps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayTempsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayTempsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gcatania/holidaytemps/HolidayTempsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gcatania.holidaytemps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayTempsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

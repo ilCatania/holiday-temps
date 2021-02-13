c772cfcf4e9c373cb0466142ea46bf4d1807e484
Gabriele Catania <gabriele.ctn@gmail.com>
2021-02-13 21:29:18 +0000

    holiday service impl+test

--- src/main/java/it/gcatania/holidaytemps/impl/HolidayServiceImpl.java
+++ src/main/java/it/gcatania/holidaytemps/impl/HolidayServiceImpl.java
@@ -2,15 +2,38 @@ package it.gcatania.holidaytemps.impl;
 
 import it.gcatania.holidaytemps.model.Holiday;
 import it.gcatania.holidaytemps.service.HolidayService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class HolidayServiceImpl implements HolidayService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${holiday.ws.host}")
+    private String wsHost;
+
     @Override
     public List<Holiday> holidays(String city, LocalDate from, LocalDate to) {
-        return null;
+        Map<String, ?> holidayData = restTemplate.getForObject(wsHost + "/bank-holidays.json", Map.class);
+        // TODO figure out correct region from city, improve deserialization, error handling
+        Map<String, ?> holidaysForRegion = (Map<String, ?>) holidayData.get("england-and-wales");
+        List<Map<String, String>> holidayEntries = (List<Map<String, String>>) holidaysForRegion.get("events");
+        List<Holiday> holidays = holidayEntries
+                .stream()
+                .map(m -> new Holiday(m.get("title"), LocalDate.parse(m.get("date"))))
+                .sorted(Comparator.comparing(Holiday::getDate))
+                .collect(Collectors.toList());
+        return Collections.unmodifiableList(holidays);
     }
 }
---
+++ src/test/java/it/gcatania/holidaytemps/HolidayServiceTest.java
@@ -0,0 +1,53 @@
+package it.gcatania.holidaytemps;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import it.gcatania.holidaytemps.model.Holiday;
+import it.gcatania.holidaytemps.service.HolidayService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = HolidayTempsApplication.class, initializers = WireMockInitializer.class)
+public class HolidayServiceTest {
+
+    @Autowired
+    private WireMockServer wireMockServer;
+
+    @Autowired
+    private HolidayService holidayService;
+
+    @Test
+    public void testGetHolidays() {
+        // set up mock webservice
+        wireMockServer.stubFor(WireMock.get("/bank-holidays.json")
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                        .withBodyFile("bank-holidays.json")));
+
+        //verify service call
+        List<Holiday> expected = Arrays.asList(
+                new Holiday("Christmas", LocalDate.parse("2020-12-25")),
+                new Holiday("Boxing Day", LocalDate.parse("2020-12-26")),
+                new Holiday("New Year's Day", LocalDate.parse("2021-01-01"))
+        );
+        List<Holiday> actual = holidayService.holidays(
+                "London",
+                LocalDate.parse("2020-12-01"),
+                LocalDate.parse("2021-01-01"));
+        assertThat(actual, is(equalTo(expected)));
+    }
+}
